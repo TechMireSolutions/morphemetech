499abaf2ace3077bce43071aec2fa763af89492d
Syed Mohib <mohibali.tms@gmail.com>
2026-07-16 13:10:21 +0500

    chore: update database credentials in db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost"; // Tumhara server name, local development ke liye 'localhost' hi rahega
-$username = "u703068112_morphemetech"; // Tumhara MySQL username
-$password = "Morphemetech110$$"; // Tumhara MySQL password
-$dbname = "u703068112_morphemetech"; // Database ka naam jo tumne banaya hai
+$username = "morphemetech"; // Tumhara MySQL username
+$password = "569ad51aa04f6"; // Tumhara MySQL password
+$dbname = "morphemetech"; // Database ka naam jo tumne banaya hai
 
 // Connection create karein
 $conn = new mysqli($servername, $username, $password, $dbname);
